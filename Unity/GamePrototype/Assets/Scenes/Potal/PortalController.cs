2e38d2935d7899e1b3f7a9189a1d6eddf376bc01
Hoanh Nguyen <103208463+Mrkkzbanana@users.noreply.github.com>
2023-09-25 12:34:15 -0700

    added GUI for portal

--- Unity/GamePrototype/Assets/Scenes/Potal/PortalController.cs
+++ Unity/GamePrototype/Assets/Scenes/Potal/PortalController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PortalController : MonoBehaviour
 {
+    public GameObject textPopUp;
+    public float rotationSpeed = 5f;
     public List<Transform> destinations;
     private bool playerInRange = false;
     private bool playerPressKey = false;
@@ -13,14 +15,29 @@ public class PortalController : MonoBehaviour
     {
         var keyboard = Keyboard.current;
 
+
+        //Alway facing player
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null && playerInRange)
+        {
+            Vector3 directionToPlayer = player.transform.position - textPopUp.transform.position;
+            Quaternion rotationToPlayer = Quaternion.LookRotation(directionToPlayer);
+            rotationToPlayer *= Quaternion.Euler(0, 180, 0); // Add 180 degrees rotation around the Y axis
+            textPopUp.transform.rotation = Quaternion.Slerp(textPopUp.transform.rotation, rotationToPlayer, Time.deltaTime * rotationSpeed);
+        }
+
+
         if (playerInRange && keyboard.fKey.wasPressedThisFrame)
         {
             Debug.Log("F Key Pressed");
+            string destinationText = "";
             for (int i = 0; i < destinations.Count; i++)
             {
                 Debug.Log("Press " + i + " to teleport to destination " + i);
+                destinationText += $"{i + 1} for destination {i + 1}\n ";
             }
             playerPressKey = true;
+            textPopUp.GetComponent<TextMesh>().text = destinationText;
         }
 
         if (playerPressKey && playerInRange)
@@ -30,6 +47,7 @@ public class PortalController : MonoBehaviour
                 if (keyboard[Key.Digit1 + j].wasPressedThisFrame)
                 {
                     Debug.Log(j + " Key Pressed");
+
                     TeleportPlayer(j);
                     playerPressKey = false;  // Reset the playerPressKey here
                     break;
@@ -59,6 +77,8 @@ public class PortalController : MonoBehaviour
         {
             Debug.LogError("Invalid Destination Index. Please enter a number between 0 and " + (destinations.Count - 1));
         }
+        textPopUp.GetComponent<TextMesh>().text = "Press F to use";
+        textPopUp.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
@@ -67,6 +87,7 @@ public class PortalController : MonoBehaviour
         {
             playerInRange = true;
             Debug.Log("Player Entered Portal Trigger");
+            textPopUp.SetActive(true);
         }
     }
 
@@ -76,6 +97,8 @@ public class PortalController : MonoBehaviour
         {
             playerInRange = false;
             Debug.Log("Player Exited Portal Trigger");
+            textPopUp.GetComponent<TextMesh>().text = "Press F to use";
+            textPopUp.SetActive(false);
         }
     }
 }
