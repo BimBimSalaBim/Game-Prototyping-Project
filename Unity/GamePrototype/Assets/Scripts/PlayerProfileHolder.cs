83794fcebc103b385a83b683564513a134be6f9c
Hoanh Nguyen <103208463+Mrkkzbanana@users.noreply.github.com>
2023-10-02 14:12:46 -0700

    Profile GUI improvement
    GUI work in progress.

--- Unity/GamePrototype/Assets/Scripts/PlayerProfileHolder.cs
+++ Unity/GamePrototype/Assets/Scripts/PlayerProfileHolder.cs
@@ -2,8 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 
 public class PlayerProfileHolder : MonoBehaviour
 {
     public GameObject[] userProfiles = new GameObject[3];
+    public int selectedProfile = 0;
+
+    public void NextProfile()
+    {
+        Debug.Log("Button clicked.");
+        userProfiles[selectedProfile].SetActive(false);
+        selectedProfile = (selectedProfile + 1) % userProfiles.Length;
+        userProfiles[selectedProfile].SetActive(true);
+       
+    }
+
+    public void PreviousProfile()
+    {
+        Debug.Log("Button clicked.");
+        userProfiles[selectedProfile].SetActive(false);
+        selectedProfile--;
+        if (selectedProfile < 0)
+        {
+            selectedProfile += userProfiles.Length;
+        }
+        userProfiles[selectedProfile].SetActive(true);
+    }
+
+    public void Sellect()
+    {
+        //doing some thing to the sellecting profile.
+        Debug.Log("Profile sellected!");
+    }
+    public void View()
+    {
+        Debug.Log("Profile Viewer!");
+        userProfiles[selectedProfile].GetComponent<Profile>().ShowStats();
+
+    }
+   
 }
--- Unity/GamePrototype/Assets/Scripts/Profile.cs
+++ Unity/GamePrototype/Assets/Scripts/Profile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Profile : MonoBehaviour
@@ -7,4 +8,11 @@ public class Profile : MonoBehaviour
     public string Name;
     public int Level;
     public GameObject Character;
+    public void ShowStats()
+    {
+        string messageOut = "";
+        messageOut = "Profile name: " + "\n" +  Name.ToString() + "\n" + "Level: " + Level;
+        GetComponent<TextMesh>().text = messageOut;
+    }
 }
+
